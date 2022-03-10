cb0f26b82ec108508bafcc0b2616cafaa811f357
William Cole <ioetbc@Williams-MacBook-Pro.local>
2022-03-10 15:06:50 +0000

    grayscale

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,1 +1,3 @@
-export const Logo = () => <h1 className="absolute left-8 top-8">title</h1>
+export const Logo = () => (
+  <h1 className="absolute left-8 top-8 text-4xl">S~R</h1>
+)
--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import { Header } from '../components/Header'
 export default function Home() {
   return (
     <>
-      <div className='absolute bg-cover bg-center bg-no-repeat bg-size h-screen w-screen bg-[url("https://images.unsplash.com/photo-1643952565578-a39d383f1dc9")]'></div>
+      <div className='absolute bg-contain md:bg-cover bg-center bg-no-repeat bg-size h-screen w-screen bg-[url("/images/bg.jpeg")]'></div>
       <Header />
     </>
   )
