6c0fca070a03a19004399434c6110f595a453a77
ioetbc <ioetbc@gmail.com>
2022-05-01 17:57:44 +0100

    investigation

--- pages/investigation-v2.jsx
+++ pages/investigation-v2.jsx
@@ -1,51 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import { images } from '../database/investigation'
+import React from 'react'
+import dynamic from 'next/dynamic'
+// import { images } from '../database/investigation'
+
+const Sketch = dynamic(() => import('react-p5'), { ssr: false })
 
 export default function Home() {
-  useEffect(() => {
-    const container = document.querySelector('.container')
-    const canvas = document.querySelector('.canvas')
-    const columnOne = document.querySelector('.one')
-    const columnTwo = document.querySelector('.two')
-    const columnThree = document.querySelector('.three')
-    const columnFour = document.querySelector('.four')
+  let mouseX = 0
+  let mouseY = 0
+  const easing = 0.09
 
-    for (let i = 0; i < images.length; i++) {
-      const imageDiv = document.createElement('div')
-      const image = document.createElement('img')
-      console.log(images[i])
-      image.setAttribute('src', `${images[i].src}`)
-      imageDiv.appendChild(image)
+  const images = [
+    {
+      src: '/images/products/granite-nude-bowls/2.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 100,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/15.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 50,
+      offsetY: 10,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/11.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 150,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/17.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 50,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/14.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 120,
+      offsetY: 20,
+    },
+  ]
 
-      if (i < 5) {
-        columnOne.appendChild(imageDiv)
-      } else if (i < 9) {
-        columnTwo.appendChild(imageDiv)
-      } else if (i < 13) {
-        columnThree.appendChild(imageDiv)
-      } else {
-        columnFour.appendChild(imageDiv)
+  const galleryWidth = images.reduce(
+    (previousValue, currentValue) =>
+      previousValue + currentValue.width + currentValue.offsetX,
+    0,
+  )
+
+  const galleryHeight = images.reduce(
+    (previousValue, currentValue) =>
+      previousValue + currentValue.height + currentValue.offsetY,
+    0,
+  )
+
+  const preload = (p5) => {
+    images.forEach((image, index) => {
+      p5.loadImage(image.src, (loadedImage) => {
+        images[index].src = loadedImage
+      })
+    })
+  }
+
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(window.innerWidth, window.innerHeight).parent(
+      canvasParentRef,
+    )
+    // p5.noCursor()
+  }
+
+  const draw = (p5) => {
+    p5.background(255, 254, 242)
+    const targetX = p5.mouseX
+    const dx = targetX - mouseX
+    mouseX += dx * easing
+    const targetY = p5.mouseY
+    const dy = targetY - mouseY
+    mouseY += dy * easing
+    for (let y = 0; y < images.length; y++) {
+      for (let x = 0; x < images.length; x++) {
+        p5.image(
+          images[x].src,
+          images[x].width * x -
+            galleryWidth / 2 +
+            mouseX +
+            images[x].offsetX * x,
+          images[x].height * y -
+            galleryHeight / 2 +
+            mouseY +
+            images[x].offsetY * y,
+          images[x].width,
+          images[x].height,
+        )
       }
     }
+  }
 
-    window.addEventListener('mousemove', (e) => {
-      const easing = 1.09
-      console.log(e.clientX)
-      const x = e.clientX * easing
-      const y = e.clientY * easing
-
-      // canvas.style.transform = `translate(-${x}px, -${y}px)`
-      canvas.style.transform = `translate3d(-${x}px, -${y}px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)`
-    })
-  }, [])
-  return (
-    <div className="container">
-      <div className="canvas">
-        <div className="column one"></div>
-        <div className="column two"></div>
-        <div className="column three"></div>
-        <div className="column four"></div>
-      </div>
-    </div>
-  )
+  return <Sketch setup={setup} draw={draw} preload={preload} />
 }
