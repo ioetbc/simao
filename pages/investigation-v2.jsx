0b88636d4521c3468767d45d1ea9ba6f150f9e48
ioetbc <ioetbc@gmail.com>
2022-05-02 14:30:25 +0100

    fix the gallery so its start from the centre

--- pages/investigation-v2.jsx
+++ pages/investigation-v2.jsx
@@ -14,8 +14,10 @@ export default function Home() {
       src: '/images/products/granite-nude-bowls/2.jpg',
       width: 326,
       height: 492,
-      offsetX: 200,
+      offsetX: 0,
       offsetY: 50,
+      column: 0,
+      row: 1,
     },
     {
       src: '/images/products/granite-nude-bowls/15.jpg',
@@ -23,6 +25,8 @@ export default function Home() {
       height: 492,
       offsetX: 100,
       offsetY: 10,
+      column: 1,
+      row: 1,
     },
     {
       src: '/images/products/granite-nude-bowls/11.jpg',
@@ -30,6 +34,8 @@ export default function Home() {
       height: 492,
       offsetX: 100,
       offsetY: 150,
+      column: 2,
+      row: 1,
     },
     {
       src: '/images/products/granite-nude-bowls/17.jpg',
@@ -37,20 +43,109 @@ export default function Home() {
       height: 492,
       offsetX: 100,
       offsetY: 50,
+      column: 3,
+      row: 1,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/17.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 50,
+      column: 4,
+      row: 1,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/2.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 200,
+      offsetY: 50,
+      column: 0,
+      row: 2,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/15.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 10,
+      column: 1,
+      row: 2,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/11.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 150,
+      column: 2,
+      row: 2,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/17.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 50,
+      column: 3,
+      row: 2,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/17.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 50,
+      column: 4,
+      row: 2,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/2.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 200,
+      offsetY: 50,
+      column: 0,
+      row: 3,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/15.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 10,
+      column: 1,
+      row: 3,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/11.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 150,
+      column: 2,
+      row: 3,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/17.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 50,
+      column: 3,
+      row: 3,
+    },
+    {
+      src: '/images/products/granite-nude-bowls/17.jpg',
+      width: 326,
+      height: 492,
+      offsetX: 100,
+      offsetY: 50,
+      column: 4,
+      row: 3,
     },
   ]
-
-  const galleryWidth = images.reduce(
-    (previousValue, currentValue) =>
-      previousValue + currentValue.width + currentValue.offsetX,
-    0,
-  )
-
-  const galleryHeight = images.reduce(
-    (previousValue, currentValue) =>
-      previousValue + currentValue.height + currentValue.offsetY,
-    0,
-  )
 
   const preload = (p5) => {
     images.forEach((image, index) => {
@@ -64,40 +159,44 @@ export default function Home() {
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(
       canvasParentRef,
     )
-    // p5.noCursor()
   }
 
-  const getRow = ({ index }) => {
-    if (index <= 4) {
-      return 1
-    } else if (index > 4 && index <= 8) {
-      return 2
-    } else {
-      return 3
+  let galleryWidth = 0
+  let galleryHeight = 0
+
+  const fatReducer = images.reduce((_, currentValue) => {
+    if (currentValue.row === 2) {
+      galleryWidth = galleryWidth + currentValue.width
+      galleryHeight = galleryHeight + currentValue.height
     }
-  }
+    return {
+      width: galleryWidth,
+      height: galleryHeight,
+    }
+  }, 0)
 
   const draw = (p5) => {
     p5.background(255, 254, 242)
     const targetX = p5.mouseX
     const dx = targetX - mouseX
-    mouseX += dx * easing * 2
-    const targetY = p5.mouseY * 3
+    const targetY = p5.mouseY
     const dy = targetY - mouseY
-    mouseY += dy * easing * 2
-    console.log({ mouseX: p5.mouseX, mouseY: p5.mouseY })
-    // const constrainedX = p5.constrain(mouseX, 400, 1200)
-    // const constrainedY = p5.constrain(mouseY, 0, 1200)
 
-    for (let y = 0; y < images.length; y++) {
-      for (let x = 0; x < images.length; x++) {
-        const { src, offsetX, offsetY, width, height } = images[x]
+    mouseX += dx * easing
+    mouseY += dy * easing
 
-        const left = width * x + -mouseX + offsetX * x + window.innerWidth / 3
-        const top = height * y + -mouseY + offsetY * y + window.innerHeight / 3
+    const mouseXOffset = mouseX - p5.width / 2
+    const mouseYOffset = mouseY - p5.height / 2
+    const shiftgalleryX = (p5.width - fatReducer.width) / 2
+    const shiftgalleryY = (p5.height - fatReducer.height) / 2
 
-        p5.image(src, left, top, width, height)
-      }
+    for (let x = 0; x < images.length; x++) {
+      const { src, offsetX, offsetY, width, height, column, row } = images[x]
+
+      const left = width * column + shiftgalleryX + -mouseXOffset
+      const top = height * row + shiftgalleryY + -mouseYOffset
+
+      p5.image(src, left, top, width, height)
     }
   }
 
--- pages/investigation.jsx
+++ pages/investigation.jsx
@@ -38,157 +38,132 @@ export default function Home() {
     x += dx * easing
     y += dy * easing
 
-    // const topRowYPosition = images[0].height * 2
-    // const middleRowYPosition = y - images[0].height / 2
-    // const bottomRowYPosition = y + images[0].height
-
-    // images.forEach((image, index) => {
-    //   const prevImage = index === 0 ? 0 : index - 1
-
-    //   let yPosition
-
-    //   if (index <= 4) {
-    //     yPosition = images[0].height * 2
-    //   } else if (index > 4 && index <= 9) {
-    //     yPosition = y - images[0].height / 2
-    //   } else {
-    //     yPosition = y + images[0].height
-    //   }
-    //   p5.image(
-    //     image.src,
-    //     x - prevImage.width - prevImage.width / 2 - 100,
-    //     yPosition - image.offsetY,
-    //     image.width,
-    //     image.height,
-    //   )
-    // })
-
     // top
-    const topLevelY = y - images[0].height * 2
+    // const topLevelY = y - images[0].height * 2
     // top middle
-    p5.image(
-      images[0].src,
-      -x - images[0].width / 2,
-      topLevelY - images[0].offsetY,
-      images[0].width,
-      images[0].height,
-    )
-    // top middle left
-    p5.image(
-      images[1].src,
-      -x - images[0].width - images[0].width / 2 - 100,
-      topLevelY - images[1].offsetY,
-      images[1].width,
-      images[1].height,
-    )
-    // top far left
-    p5.image(
-      images[2].src,
-      -x - images[1].width - images[1].width - images[1].width / 2 - 150 - 150,
-      topLevelY - images[2].offsetY,
-      images[2].width,
-      images[2].height,
-    )
-    // top middle right
-    p5.image(
-      images[3].src,
-      -x + images[2].width - images[2].width / 2 + 200,
-      topLevelY - images[3].offsetY,
-      images[3].width,
-      images[3].height,
-    )
-    // top far right
-    p5.image(
-      images[4].src,
-      -x + images[3].width + images[3].width - images[3].width / 2 + 250 + 250,
-      topLevelY - images[4].offsetY,
-      images[4].width,
-      images[4].height,
-    )
+    // p5.image(
+    //   images[0].src,
+    //   -x - images[0].width / 2,
+    //   topLevelY - images[0].offsetY,
+    //   images[0].width,
+    //   images[0].height,
+    // )
+    // // top middle left
+    // p5.image(
+    //   images[1].src,
+    //   -x - images[0].width - images[0].width / 2 - 100,
+    //   topLevelY - images[1].offsetY,
+    //   images[1].width,
+    //   images[1].height,
+    // )
+    // // top far left
+    // p5.image(
+    //   images[2].src,
+    //   -x - images[1].width - images[1].width - images[1].width / 2 - 150 - 150,
+    //   topLevelY - images[2].offsetY,
+    //   images[2].width,
+    //   images[2].height,
+    // )
+    // // top middle right
+    // p5.image(
+    //   images[3].src,
+    //   -x + images[2].width - images[2].width / 2 + 200,
+    //   topLevelY - images[3].offsetY,
+    //   images[3].width,
+    //   images[3].height,
+    // )
+    // // top far right
+    // p5.image(
+    //   images[4].src,
+    //   -x + images[3].width + images[3].width - images[3].width / 2 + 250 + 250,
+    //   topLevelY - images[4].offsetY,
+    //   images[4].width,
+    //   images[4].height,
+    // )
 
     // MIDDLE
     const msiddleYLevel = y - images[0].height / 2
     p5.image(
       images[0].src,
-      -x - images[0].width / 2,
+      -x + window.innerWidth / 2,
       msiddleYLevel,
       images[0].width,
       images[0].height,
     )
     // middle left
-    p5.image(
-      images[1].src,
-      -x - images[0].width - images[0].width / 2 - 200,
-      msiddleYLevel,
-      images[1].width,
-      images[1].height,
-    )
-    // middle far left
-    p5.image(
-      images[2].src,
-      -x - images[1].width - images[1].width - images[1].width / 2 - 400,
-      msiddleYLevel,
-      images[2].width,
-      images[2].height,
-    )
-    // middle right
-    p5.image(
-      images[3].src,
-      -x + images[2].width - images[2].width / 2 + 300,
-      msiddleYLevel,
-      images[3].width,
-      images[3].height,
-    )
-    // far right
-    p5.image(
-      images[4].src,
-      -x + images[3].width + images[3].width - images[3].width / 2 + 700,
-      msiddleYLevel,
-      images[4].width,
-      images[4].height,
-    )
+    // p5.image(
+    //   images[1].src,
+    //   -x - images[0].width - images[0].width / 2 - 200,
+    //   msiddleYLevel,
+    //   images[1].width,
+    //   images[1].height,
+    // )
+    // // middle far left
+    // p5.image(
+    //   images[2].src,
+    //   -x - images[1].width - images[1].width - images[1].width / 2 - 400,
+    //   msiddleYLevel,
+    //   images[2].width,
+    //   images[2].height,
+    // )
+    // // middle right
+    // p5.image(
+    //   images[3].src,
+    //   -x + images[2].width - images[2].width / 2 + 300,
+    //   msiddleYLevel,
+    //   images[3].width,
+    //   images[3].height,
+    // )
+    // // far right
+    // p5.image(
+    //   images[4].src,
+    //   -x + images[3].width + images[3].width - images[3].width / 2 + 700,
+    //   msiddleYLevel,
+    //   images[4].width,
+    //   images[4].height,
+    // )
 
     // BOTOM
-    const topYLevel = y + images[0].height
-    p5.image(
-      images[0].src,
-      -x - images[0].width / 2,
-      topYLevel - 200,
-      images[0].width,
-      images[0].height,
-    )
-    // middle left
-    p5.image(
-      images[1].src,
-      -x - images[0].width - images[0].width / 2 - 150,
-      topYLevel - 100,
-      images[1].width,
-      images[1].height,
-    )
-    // middle far left
-    p5.image(
-      images[2].src,
-      -x - images[1].width - images[1].width - images[1].width / 2 - 600,
-      topYLevel - 150,
-      images[2].width,
-      images[2].height,
-    )
-    // middle right
-    p5.image(
-      images[3].src,
-      -x + images[2].width - images[2].width / 2 + 350,
-      topYLevel,
-      images[3].width,
-      images[3].height,
-    )
-    // far right
-    p5.image(
-      images[4].src,
-      -x + images[3].width + images[3].width - images[3].width / 2 + 600,
-      topYLevel - 250,
-      images[4].width,
-      images[4].height,
-    )
+    // const topYLevel = y + images[0].height
+    // p5.image(
+    //   images[0].src,
+    //   -x - images[0].width / 2,
+    //   topYLevel - 200,
+    //   images[0].width,
+    //   images[0].height,
+    // )
+    // // middle left
+    // p5.image(
+    //   images[1].src,
+    //   -x - images[0].width - images[0].width / 2 - 150,
+    //   topYLevel - 100,
+    //   images[1].width,
+    //   images[1].height,
+    // )
+    // // middle far left
+    // p5.image(
+    //   images[2].src,
+    //   -x - images[1].width - images[1].width - images[1].width / 2 - 600,
+    //   topYLevel - 150,
+    //   images[2].width,
+    //   images[2].height,
+    // )
+    // // middle right
+    // p5.image(
+    //   images[3].src,
+    //   -x + images[2].width - images[2].width / 2 + 350,
+    //   topYLevel,
+    //   images[3].width,
+    //   images[3].height,
+    // )
+    // // far right
+    // p5.image(
+    //   images[4].src,
+    //   -x + images[3].width + images[3].width - images[3].width / 2 + 600,
+    //   topYLevel - 250,
+    //   images[4].width,
+    //   images[4].height,
+    // )
   }
 
   return <Sketch setup={setup} draw={draw} preload={preload} />
